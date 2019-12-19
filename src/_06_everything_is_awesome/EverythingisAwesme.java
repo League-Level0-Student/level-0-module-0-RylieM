ad5365baac651d5a27d230c33a50d0c0914fc6f5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-18 18:20:53 -0800

    12/18/19

---
+++ src/_06_everything_is_awesome/EverythingisAwesme.java
@@ -0,0 +1,42 @@
+package _06_everything_is_awesome;
+
+import javax.swing.JOptionPane;
+
+public class EverythingisAwesme {
+
+		
+	
+	c
+	
+	int cat=2;
+	
+	JOptionPane.showInputDialog(null, "What is Awesome?");
+	
+	
+	JOptionPane.showMessageDialog(null,"cat is awesome");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+	private void JOptionPane(Object object, String string) {
+		// TODO Auto-generated method stub
+		
+	}	
+}
---
+++ src/_07_last_summer/Last_Summer.java
@@ -0,0 +1,30 @@
+package _07_last_summer;
+
+import javax.swing.JOptionPane;
+
+public class Last_Summer {
+
+	public static void main(String [] args)	{
+	
+	int hat=2;
+	
+	JOptionPane.showInputDialog(null, "What is your name?");
+	
+	JOptionPane.showMessageDialog(null, "I know that you swam last summer "+hat+" Muhhahahha!!");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+}
